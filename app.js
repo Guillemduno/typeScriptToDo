67656b747e9d0be99a07112f181a70e14e918a64
Guillem <guillemduno@gmail.com>
2018-10-09 14:51:14 +0200

    fixed tsc error

---
+++ app.js
@@ -0,0 +1,6 @@
+var person = {
+    name: "Jenny",
+    sex: "female",
+    age: 23
+};
+console.log("This is: " + person.name + " and is a " + person.sex + ", and her age is " + person.age);
---
+++ app.ts
@@ -0,0 +1,8 @@
+
+const person = {
+    name: "Jenny",
+    sex: "female",
+    age: 23
+}
+
+console.log(`This is: ${person.name} and is a ${person.sex}, and her age is ${person.age}`);
